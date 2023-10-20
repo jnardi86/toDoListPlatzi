203e6ecbc76c50372f88673c95d01fe464016e58
jnardi86 <julian.nardi@gmail.com>
2023-10-20 18:32:09 -0300

    Last Initial Design

--- src/components/toDoForm/index.jsx
+++ src/components/toDoForm/index.jsx
@@ -1,54 +1,54 @@
-import React, {useContext, useState} from 'react'
+import React, { useContext, useState } from 'react'
 import { TodoContext } from '../../toDoContext';
 import './toDoForm.css'
 
 const TodoForm = () => {
 
     const {
-      setOpenModal,
-      addTodo,
+        setOpenModal,
+        addTodo,
     } = useContext(TodoContext);
 
     const [newTodoValue, setNewTodoValue] = useState('');
 
-    const onSubmit = (event)=>{
+    const onSubmit = (event) => {
         event.preventDefault();
         addTodo(newTodoValue);
         setOpenModal(false);
     }
 
-    const onCancel = (event)=>{
+    const onCancel = (event) => {
         event.preventDefault();
         setOpenModal(false);
     }
 
-    const onChange = (event)=>{
+    const onChange = (event) => {
         setNewTodoValue(event.target.value);
     }
 
 
     return (
         <form onSubmit={onSubmit}>
-            <label>Agregar nueva Tarea</label>
-            <textarea 
-            placeholder='Nueva tarea'
-            value={newTodoValue}
-            onChange={onChange}
-            required
-            ></textarea>
-            <div className='TodoForm-buttonContainer'>
-                <button
-                type='button' 
-                className='TodoForm-button TodoForm-button--cancel'
-                onClick={onCancel}>
-                    Cancelar
-                </button>
-                <button 
-                type='submit'
-                className='TodoForm-button TodoForm-button--add'>
-                    Agregar
-                </button>
-            </div>
+                <label>Agregar nueva Tarea</label>
+                <textarea
+                    placeholder='Nueva tarea'
+                    value={newTodoValue}
+                    onChange={onChange}
+                    required
+                ></textarea>
+                <div className='TodoForm-buttonContainer'>
+                    <button
+                        type='button'
+                        className='TodoForm-button TodoForm-button--cancel'
+                        onClick={onCancel}>
+                        Cancelar
+                    </button>
+                    <button
+                        type='submit'
+                        className='TodoForm-button TodoForm-button--add'>
+                        Agregar
+                    </button>
+                </div>
         </form>
     )
 }
